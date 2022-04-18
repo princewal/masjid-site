ebf0fd4f878c63e6c450fd3e204f34c7f193c8e2
princewal <princewal@hotmail.com>
2022-04-18 01:30:06 -0400

    added prayer time in console
    Test the prayer time in console.

--- src/script/main.js
+++ src/script/main.js
@@ -1,3 +1,9 @@
+AOS.init({
+  easing: 'fade-up',
+  duration: 700,
+});
+
+
 
 async function fetchHijriJSON() {
   let todayDate = getCurrentDate()
@@ -11,8 +17,22 @@ function getCurrentDate() {
   let cDay = currentDate.getDate()
   let cMonth = currentDate.getMonth() + 1
   let cYear = currentDate.getFullYear()
-  return  cDay + "-" + cMonth + "-" + cYear
+  return cDay + "-" + cMonth + "-" + cYear
 }
+
+const request = (url, params = {}, method = 'GET') => {
+  let options = {
+    method
+  };
+  if ('GET' === method) {
+    url += '?' + (new URLSearchParams(params)).toString();
+  } else {
+    options.body = JSON.stringify(params);
+  }
+
+  return fetch(url, options).then(response => response.json());
+};
+
 
 
 window.addEventListener('load', async (e) => {
@@ -23,4 +43,16 @@ window.addEventListener('load', async (e) => {
   let hYear = todayHijri.year
 
   document.querySelector('header .islamic-date span').innerHTML = `${hDay} ${hMonth} ${hYear}`
+
+  let prayerTime = request('https://api.aladhan.com/v1/calendarByCity', {
+      city: 'Toronto',
+      country: 'Canada',
+      month: new Date().getMonth() + 1,
+      year: new Date().getFullYear(),
+      method: 2,
+    })
+    .then(response => {
+      // Do something with response.
+      console.log('prayer time', response)
+    });
 })
